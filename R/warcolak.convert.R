1f443eacd53ffc6e0bf6d9ba2b311b26da8f914e
Neville Jackson <nanddjackson@bigpond.com>
2015-08-16 00:00:00 +0000

    version 1.6-3

--- R/warcolak.convert.R
+++ R/warcolak.convert.R
@@ -3,7 +3,7 @@ function(w){
 # warcolak.convert() - convert warcolak data file to format required for a 
 #                      dataframe for dmm() or mdf()
   df <- w
-  colnames(df) <- c("Id","SId","DId","Sex","Trait1","Trait2")
+  colnames(df) <- c("Id","SId","DId","Sex","Trait1","Trait2",colnames(w)[7:13])
   df[,"Id"] <- w[,"ID"]
   df[,"SId"] <- w[,"Sire"]
   df[,"DId"] <- w[,"Dam"]
